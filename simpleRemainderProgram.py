177df22387da4ca9e73e03baa9938f0a5e0542da
Ruth Alice <59374518+PrincessYue@users.noreply.github.com>
2020-03-02 15:48:27 -0500

    Add files via upload

---
+++ simpleRemainderProgram.py
@@ -0,0 +1,7 @@
+def main():
+    smallerInt = int(input('Please enter an integer. '))
+    largerInt = int(input('Please enter a larger integer. '))
+    remainderOfInt = largerInt % smallerInt
+    print('The remainder is', remainderOfInt)
+main()
+            
