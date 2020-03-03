8259707b960bb3ba319fc78daeb7ddd551c9727a
Ruth Alice <59374518+PrincessYue@users.noreply.github.com>
2020-03-03 13:09:55 -0500

    simple sum decimals

---
+++ simpleSumDecimals.py
@@ -0,0 +1,9 @@
+def main():
+    sumOfAll = 0
+    for num in range(5):
+        num = float(input('Please enter a number that includes a decimal. '))
+        sumOfAll = sumOfAll + num
+    print('The sum of all the numbers you entered is', sumOfAll)
+main()
+        
+        
